c0d334ff03d4d5193412fd3e573de68cad6185d0
CaptKrasno <kkrasnosky@gmail.com>
2022-02-28 21:29:11 -0500

    Update cmake to include QRC files... example usage in hello_gui_node

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # template_gui_package
 A simple example/template for making ROS QT GUIs
+
+## For a full explaination.
+see this video:  https://youtu.be/Cg1DaNFnZyY
--- nodes/hello_gui_node.cpp
+++ nodes/hello_gui_node.cpp
@@ -1,4 +1,5 @@
 #include <QApplication>
+#include <QIcon>
 #include "hello_gui.h"
 
 
@@ -9,8 +10,15 @@ int main(int argc, char *argv[])
   QApplication a(argc, argv);
 
   HelloGui w;
+
+  // set the window title as the node name
   w.setWindowTitle(QString::fromStdString(
                        ros::this_node::getName()));
+
+  // load the icon from our qrc file and set it as the application icon
+  QIcon icon(":/icons/my_gui_icon.png");
+  w.setWindowIcon(icon);
+
   w.show();
   return a.exec();
 }
